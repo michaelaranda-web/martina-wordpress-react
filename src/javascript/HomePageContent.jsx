15b9a330743f106bab5dcdcaf491a0e2aba0f7eb
Michael Aranda <michaelaranda0.web@gmail.com>
2018-02-19 22:17:55 +0000

    Display media items fetched from WP on Home Page.

--- src/javascript/HomePageContent.jsx
+++ src/javascript/HomePageContent.jsx
@@ -2,17 +2,36 @@ import React, { Component } from 'react';
 import { MEDIA_URL } from '../config';
 
 class HomePageContent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      mediaItemLinks: []
+    };
+  }
+  
   componentDidMount() {
+    var self = this;
+    
     fetch(MEDIA_URL).then(function(response) {
       return response.json();
     }).then(function(response) {
-      console.log(response);
+      let fetchedMediaItemLinks = response.map(function(mediaItem) {
+        return mediaItem.source_url;
+      })
+      self.setState({mediaItemLinks: fetchedMediaItemLinks});
     });
   }
   
   render() {
     return (
       <div className="home-page-content">
+        {
+          this.state.mediaItemLinks.map(function(mediaItemLink) {
+            return (
+              <img src={mediaItemLink} />
+            );
+          })
+        }
       </div>
     );
   }
