6c039d7c22099984ab912b8fa6107a219affac46
Michael Aranda <michaelaranda0.web@gmail.com>
2018-02-21 04:21:01 +0000

    Adds header and links to NavBar

--- src/javascript/NavBar.jsx
+++ src/javascript/NavBar.jsx
@@ -4,6 +4,25 @@ class NavBar extends Component {
   render() {
     return (
       <div className="main-nav-bar">
+        <div className="container">
+          <h1 className="nav-bar-title">Martina Lo</h1>
+          
+          <div>
+            <a href="#">Home</a>
+          </div>
+          <div>
+            <a href="#">Storyboard</a>
+          </div>
+          <div>
+            <a href="#">Life Drawings</a>
+          </div>
+          <div>
+            <a href="#">Graphics & Animation</a>
+          </div>
+          <div>
+            <a href="#">Comics</a>
+          </div>
+        </div>
       </div>
     );
   }
