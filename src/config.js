6f157d4cd19722081613c364087b4fe7f1900a28
Michael Aranda <michaelaranda0.web@gmail.com>
2018-03-09 05:48:10 +0000

    Layout for Home Page, fetching content from WP Home Page post.

--- src/config.js
+++ src/config.js
@@ -4,3 +4,4 @@ export const COMICS_URL = WP_HEADLESS_URL + '/wp-json/wp/v2/posts?categories=2';
 export const ANIMATIONS_URL = WP_HEADLESS_URL + '/wp-json/wp/v2/posts?categories=4';
 export const LIFE_DRAWINGS_URL = WP_HEADLESS_URL + '/wp-json/wp/v2/posts?categories=5';
 export const STORYBOARDS_URL = WP_HEADLESS_URL + '/wp-json/wp/v2/posts?categories=6';
+export const HOME_PAGE_CONTENT_URL = WP_HEADLESS_URL + '/wp-json/wp/v2/posts?categories=7';
--- src/javascript/content_pages/HomePage.jsx
+++ src/javascript/content_pages/HomePage.jsx
@@ -1,37 +1,67 @@
 import React, { Component } from 'react';
-import { MEDIA_URL } from '../../config';
+import FetchSpinner from '../FetchSpinner';
+import { getImageLinksForPosts } from '../parsers/WordPressParser';
+import { HOME_PAGE_CONTENT_URL } from '../../config';
 
 class HomePage extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      mediaItemLinks: []
+      columnOneLinks: [],
+      columnTwoLinks: []
     };
   }
   
-  componentDidMount() {
-    var self = this;
+  setMediaItems(response) {
+    var homeImageLinks = getImageLinksForPosts(response);
+    var flattenedHomeImageLinks = [].concat.apply([], homeImageLinks);
     
-    fetch(MEDIA_URL).then(function(response) {
-      return response.json();
-    }).then(function(response) {
-      let fetchedMediaItemLinks = response.map(function(mediaItem) {
-        return mediaItem.source_url;
-      })
-      self.setState({mediaItemLinks: fetchedMediaItemLinks});
-    });
+    var columnOneLinks = [];
+    var columnTwoLinks = [];
+    
+    flattenedHomeImageLinks.map((imageLink, i) => {
+      if (i % 2 === 0) {
+        columnOneLinks.push(imageLink);
+      } else {
+        columnTwoLinks.push(imageLink);
+      }
+    })
+    
+    this.setState({columnOneLinks: columnOneLinks});
+    this.setState({columnTwoLinks: columnTwoLinks});
   }
   
   render() {
     return (
       <div className="home-page">
-        {
-          this.state.mediaItemLinks.map(function(mediaItemLink, i) {
-            return (
-              <img key={i} src={mediaItemLink} alt={`home-page-image-${i}`}/>
-            );
-          })
-        }
+        <FetchSpinner requestUrl={HOME_PAGE_CONTENT_URL} onFetchSuccess={this.setMediaItems.bind(this)}>
+          <div className="column-one">
+            <div className="flex-container">
+              {
+                this.state.columnOneLinks.map((link, i) => {
+                  return (
+                    <div className="col-one-item" key={i}>
+                      <img src={link} />
+                    </div>
+                  ) 
+                })
+              }
+            </div>
+          </div>
+          <div className="column-two">
+            <div className="flex-container">
+              {
+                this.state.columnTwoLinks.map((link, i) => {
+                  return (
+                    <div className="col-two-item" key={i}>
+                      <img src={link} />
+                    </div>
+                  ) 
+                })
+              }
+            </div>
+          </div>
+        </FetchSpinner>
       </div>
     );
   }
